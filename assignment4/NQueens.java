3b25925852515a5816e82a70d21841c26701c45f
G vijesh kumar <gvijesh.kumar@metacube.com>
2024-10-29 12:01:32 +0530

    added comments to assigment four files

--- assignment4/NQueens.java
+++ assignment4/NQueens.java
@@ -10,8 +10,10 @@ import java.util.Arrays;
  * right aligned \ and left aligned / 
  * for a board of size n 
  * there can be 2*(n-1)-1 diagonals containing more than one cell
- * also the right aligned diagonal of the cell can be found using the fact that 
+ * Right Aligned Diagonal of the cell can be found using the fact that 
  * for cell (r,c) if r==c then it will lie in the middle diagonal
+ * any other cell's diagonal can be calculated based on its positional difference with any (x,y) cell on row where x==y
+ * Left Aligned Diagonal is nothing different , but it is just the result from the right aligned diagonal but with the row wise rotation towards right
  */
 public class NQueens {
    
@@ -30,23 +32,32 @@ public class NQueens {
      * @return true if only the requested cell is safe and not under attack by any queen
      */
      
-    public Boolean safe(int r,int c, Boolean[][] res) {
+    private Boolean safe(int r,int c, Boolean[][] res) {
            
         int rDInd = rDiagonalIndFinder(r,c); 
         int lDInd = lDiagonalIndFinder(r,c);
         
-        if(rows[r] || columns[c])
+        if(rows[r] || columns[c]) // checking rows and columns
             return false;
-        else if(rDInd!=-1 && rDiagonals[rDInd]) {
+        else if(rDInd!=-1 && rDiagonals[rDInd]) { // checking right aligned diagonals
             return false;
         }
-        else if(lDInd!=-1 && lDiagonals[lDInd])
+        else if(lDInd!=-1 && lDiagonals[lDInd])   // checking left aligned diagonals
             return false;
         else 
             return true;
     }
     
-    public Boolean recursivelySolve(int r,int c,int q,int n,Boolean[][] board) {
+    /**
+     * 
+     * @param r row
+     * @param c column
+     * @param q remaining queens count to be placed
+     * @param n dimension of board n*n
+     * @param board holds the positions where queen has been placed
+     * @return true if any form of result was found
+     */
+    private Boolean recursivelySolve(int r,int c,int q,int n,Boolean[][] board) {
         
         if(q==0) { // all queens placed
             System.out.println("reached solution");
@@ -111,57 +122,79 @@ public class NQueens {
         return result;
     }
     
-    public int rDiagonalIndFinder(int r,int c){
+    /**
+     * 
+     * @param r row
+     * @param c columns
+     * @return the index of the right diagonal for the cell (r,c)
+     */
+    private int rDiagonalIndFinder(int r,int c){
         
 
-        int length = this.rDiagonals.length;
-        int middle = (length+1)/2-1;
-        int n = this.rows.length;
-        int ind =-1;
+        int length = this.rDiagonals.length; // finds the no of diagonals
+        int middle = (length+1)/2-1;         // the index of the middle diagonal passing through (r,c) where r==c 
+        int n = this.rows.length;             
+        int ind =-1;                        // default value used if the diagonal is just that is passes through only one cell 
         
-        if((r==0 && c==n-1) || (r==n-1 && c==0) )
+        if((r==0 && c==n-1) || (r==n-1 && c==0) )  // single diagonals cell
             ind=-1;
-        else if(r==c)
+        else if(r==c)                              // middle index for r==c
             ind=middle;
-        else if(c>r)
-            ind=middle+(c-r);
+        else if(c>r)                               // if the cell is right to index     
+            ind=middle+(c-r);                      // then c-r as the c is larger
         else 
-            ind=middle-(r-c);
-        
-        //System.out.println("row:" + r +' '+"column: "+ c+' '+"index: "+ind+" len:"+length);
-        
+            ind=middle-(r-c);                      // in this case cell is left to the index
+                                                    
+        // for simplicity we could have used  middle + Math.abs(r-c);              
+       
         return ind;
         
     }
-    public int lDiagonalIndFinder(int r,int c) {
+    
+    /**
+     * @param r row
+     * @param c columns
+     * @return the index of the left diagonal for the cell (r,c)
+     */
+    private int lDiagonalIndFinder(int r,int c) {
         
         int n = this.rows.length;
         
-        if((r==0 && c==0) || (r==n-1 && c==n-1) )
+        if((r==0 && c==0) || (r==n-1 && c==n-1) ) // single diagonals cell
             return -1;
       
-        return rDiagonalIndFinder(r,n-1-c);
+        return rDiagonalIndFinder(r,n-1-c);       // the left diagonal is index is same as the right but rotated towards right row wise
     }
     
+    /**
+     * this act as a setup for the recursive methods this 
+     * gives the method the required parameter in right way 
+     * @param n the dimension of board also the no of queens to placed
+     */
     public void solution(int n) {
         
-        if(n==0 || n==2 )
+        if(n==0 || n==2 )   // no solution exist for these dimensions
             return;
         if(n==1) {
-            this.finalBoard=new Boolean[][] {{true}};
+            this.finalBoard=new Boolean[][] {{true}}; // for dimension 1 simply put the queen at the only cell
             return;    
         }
         
-        Boolean[][] board = new Boolean[n][n];
-        this.finalBoard=new Boolean[n][n];
+        // initialize the arrays to used in the recursive function
         
-        this.rows = new Boolean[n];
+        Boolean[][] board = new Boolean[n][n];
+        this.finalBoard=new Boolean[n][n];     
+        
+        this.rows = new Boolean[n];            
         this.columns = new Boolean[n];
+        
         System.out.println("n is given as: "+ n);
-        int noOfDiagonals = 2*(n-1)-1;// 1+2*(n-2)
+        
+        int noOfDiagonals = 2*(n-1)-1;         //calculated formula 
         this.rDiagonals = new Boolean[noOfDiagonals];
         this.lDiagonals = new Boolean[noOfDiagonals];
         
+        //default value filling
         Arrays.fill(rows, false);
         Arrays.fill(columns, false);
         Arrays.fill(rDiagonals, false);
--- assignment4/knightTour.java
+++ assignment4/knightTour.java
@@ -1,20 +1,80 @@
 package assignment4;
 
+/**
+ *  This class contains method that help us find the solution for the knight tour problem and find suitable moves
+ *  A knight's tour is a sequence of moves of a knight on a chess board such that the knight visits every square only once. 
+ *  If the knight ends on a square that is one knight's move from the beginning square (so that it could tour the board again immediately,
+ *  following the same path), the tour is closed, otherwise it is open.
+ */
 public class knightTour {
 
-    int [][] finalBoard;
-  
-    public Boolean recursivelySolve(int r,int c,int k,int n,int[][] board) {
+    int [][] finalBoard; // will hold end result
+    
+    /**
+     * at the end of the finding a tour check if the tour is closed or nor
+     * @param board
+     * @param n
+     * @return true if the tour is closed
+     */
+    boolean canReachBackStart(int[][] board,int n) {
         
-        if(r<0 || c<0 || r>=n || c>=n) {
+        //final move 
+        int endRow=0; 
+        int endCol=0;
+        
+        //starting cell
+        int startRow=0;        
+        int startCol=0;
+        
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                if(board[i][j]==0) {          // start position move no
+                    startRow=i;
+                    startCol=j;
+                }
+                else if(board[i][j]==n*n-1) { // final move no is this
+                    endRow=i;
+                    endCol=j;
+                }
+            }
+        }
+        
+        // for the startPostiton to be reachable from the endPostion 
+        
+        if((Math.abs(startCol-endCol)==2 && Math.abs(startRow-endRow)==1) ||  (Math.abs(startCol-endCol)==1 && Math.abs(startRow-endRow)==2))
+            return true;
+          
+        return false;
+           
+    }
+    
+    /**
+     * this method finds the solution for knight tour problem pass the cell's row and column from where knight should start tour  
+     * @param r current row
+     * @param c current column
+     * @param k knights remaining to be placed
+     * @param n dimension of the board
+     * @param board holds the the moves played by the knight for ex. if number in the cell is 3 then knight at the end of the 3rd move it was there
+     * @param closedTour if closed tour then knight need to reach the same starting cell
+     * @return true if the solution exist for the given dimensions
+     */
+    private Boolean recursivelySolve(int r,int c,int k,int n,int[][] board,boolean closedTour) {
+        
+        System.out.println("Running");
+        System.out.println(".");
+        
+        if(r<0 || c<0 || r>=n || c>=n) { // out of range 
             return false;
         }
         
-        if(k==0){
+        if(k==0){ // base condition all the knights placed successfully
+            
+            if(closedTour && !canReachBackStart(board,n)) // if solution requires closedTour and closed tour is not possible with this setup 
+                return false;
             
             for(int i=0;i<n;i++) {
                 for(int j=0;j<n;j++) {
-                    finalBoard[i][j]=board[i][j];
+                    finalBoard[i][j]=board[i][j]; // store the solution in the finalBoard so it preserves
                 }
             }
             
@@ -24,20 +84,23 @@ public class knightTour {
         Boolean result=false;
         
         
-        if(board[r][c]==-1){
+        if(board[r][c]==-1){         // -1 means unvisited 
 
-            board[r][c]=n*n-k;
-            k--;
+            board[r][c]=n*n-k;     // the move number 
+            k--;                  // knight placed
             
-            for(int dr=-2;dr<=2;dr++){
-                for(int dc=-2;dc<=2;dc++) {
-                    if(Math.abs(dc)!=Math.abs(dr) && !(dc==0 || dr==0))
-                    result=recursivelySolve(r+dr,c+dc,k,n,board);
+            // the below loops tries all the possible moves for knight from given cell
+            
+            for(int dr=-2;dr<=2;dr++){ // dr means difference in the row if have to move
+                for(int dc=-2;dc<=2;dc++) { // dc means difference in the column if have to move
+                    
+                    if(Math.abs(dc)!=Math.abs(dr) && !(dc==0 || dr==0)) // horse moves such that dr!=dc means on the dc,dr should be 1 and other has to be 2
+                    result=recursivelySolve(r+dr,c+dc,k,n,board,closedTour);       //
                     if(result)
                         return result;
                 }
             }
-            
+            // back tracking
             k++;
             board[r][c]=-1;
             
@@ -48,18 +111,25 @@ public class knightTour {
         
     }
     
-    public void solution(int n) {
+    /**
+     * setup the parameter for calling the recursive method
+     * @param n board dimension
+     * @param closedTour if closed tour then knight need to reach the same starting cell
+     */
+    public void solution(int n,boolean closedTour) {
         
-        if(n==0 || n==2 )
+        if(n==0 || n==2 ) // no solution exist
             return;
         if(n==1) {
             this.finalBoard=new int[][] {{0}};
             return;    
         }
         
+        // initialize the arrays
         int[][] board   = new int[n][n];
         this.finalBoard = new int[n][n];
         
+        // marks the board cell as unvisited
         for(int i=0;i<n;i++) {
             for(int j=0;j<n;j++) {
                 board[i][j]=-1;
@@ -67,17 +137,20 @@ public class knightTour {
             }
         }
         
-        Boolean ans = this.recursivelySolve(0,0,n*n,n,board);
+        Boolean ans = this.recursivelySolve(0,0,n*n,n,board,closedTour);
         
          System.out.println("Does solution exist: "+ans);
     }
     
     public static void main(String[] args) {
         
+        
         knightTour k = new knightTour();
         int n=8;
-        k.solution(n);
+        boolean closedTour = true; 
+        k.solution(n,closedTour);
         
+        // evenly prints the board to console
         for(int i=0;i<n;i++) {
             for(int j=0;j<n;j++) {
                     System.out.printf("%d\t",k.finalBoard[i][j]);
