7f3fc4ad224b3573b6cf5babd1348a679c37ec9c
G vijesh kumar <gvijesh.kumar@metacube.com>
2024-12-01 00:01:54 +0530

    added layouting

--- UI/session5/js/index.js
+++ UI/session5/js/index.js
@@ -1,7 +1,8 @@
  
  const sideBarToggle=document.querySelector("#sideBarToggle");
  const sideNavCollapse=document.querySelector("#sideNavCollapse");
- 
+
+
  sideBarToggle.addEventListener('click',(e)=>{
     console.log('clicked',e)
     
@@ -16,4 +17,32 @@
         sideNavCollapse.setAttribute('data-expanded',"true");
     }
 
+    // var sideBarWidth=sideNavCollapse.offsetWidth;
+    // console.log(sideBarWidth);
+    // const r = document.querySelector(':root');
+    // r.style.setProperty('--sideBar-width',sideBarWidth);
+
  })
+
+
+ window.onload = function () {
+    var chart = new CanvasJS.Chart("chartContainer",
+        {
+
+            data: [
+                {
+                    type: "doughnut",
+                    innerRadius: 90,  // Makes the doughnut thinner
+                    dataPoints: [
+                        { y: 64, color: "#0acf97" },
+                        { y: 10, color: "#fa5c7c" },
+                        { y: 26, color: "#727cf5" }
+                    ]
+                }
+            ]
+        });
+
+    chart.render();
+}
+
+
