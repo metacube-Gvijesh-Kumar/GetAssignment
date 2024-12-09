3bcd9b9b6a9c584c0d93e8d48677111e4384db3a
G vijesh kumar <gvijesh.kumar@metacube.com>
2024-12-09 12:23:16 +0530

    added html serving in the node assignment

--- UI/session8/index.js
+++ UI/session8/index.js
@@ -1,11 +1,13 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello ji!');
-  console.log(req);
-})
+app.use(express.static("public"));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/assignment.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
